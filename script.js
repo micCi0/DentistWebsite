0da2c292ecdee7d1f0a4e079607ec5b55ab6700f
micCi0 <120597140+micCi0@users.noreply.github.com>
2024-08-13 11:40:52 +0200

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+const sr = ScrollReveal({
+    distance:"65px",
+    duration:2000,
+    delay:450,
+    reset:true
+})
+sr.reveal(".home" , {delay:200,origin:"top"});
+sr.reveal(".home-image", {origin:"right",distance:"200px"});
